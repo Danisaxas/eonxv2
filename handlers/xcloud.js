0d10a539b8f34ae08b3c03ac5cac62701fea03f1
Danisaxas <danluismay@gmail.com>
2025-06-06 20:00:11 -0400

    Update xcloud.js

--- handlers/xcloud.js
+++ handlers/xcloud.js
@@ -2,15 +2,16 @@ const texts = require('../texts/es.js');
 const buttons = require('../texts/button/es.js');
 
 module.exports = (bot, query, config) => {
-  const msg = query.message;
-  bot.answerCallbackQuery(query.id);
-  bot.editMessageText(
-    texts.xcloud_text(config, msg),
+const msg = query.message;
+bot.answerCallbackQuery(query.id);
+bot.editMessageText(
+    texts.xcloud_text(config, query.from),
     {
-      chat_id: msg.chat.id,
-      message_id: msg.message_id,
-      parse_mode: 'HTML',
-      ...buttons.xcloud_bt()
-    }
-  );
+    chat_id: query.message.chat.id,
+    message_id: query.message.message_id,
+    parse_mode: 'HTML',
+    ...buttons.xcloud_bt()
+}
+);
 };
+
